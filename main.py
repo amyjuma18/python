b22cec46a65e1afdf9a7a530b6508c26a14be8fa
amyjuma18 <jumaisboom@gmail.com>
2023-02-12 16:03:08 -0800

    variables and datatypes

---
+++ main.py
@@ -0,0 +1,31 @@
+greeting="Hello juma"
+counter = 0
+miles = 1000.0
+results = bool(True)
+
+print (counter)
+
+x = str(3)
+y = int(3)
+
+xy= '5'
+a= '5'
+c=int(xy) + int(a)
+print(c)
+print(type(xy))
+name1 = "Josephine"
+Name1 = "Paul"
+print(name1)
+fruit1, fruit2, fruit3 = 'apple','mango','avocado'
+print(fruit1)
+print(fruit2)
+print(fruit3)
+fruit4 = fruit5 = fruit6 = 'pineapple'
+print(fruit4)
+print(fruit5)
+print(fruit6)
+fruits = ('pineapple','mango','apple')
+f1,f2,f3 = fruits
+print(f1)
+print(f2)
+print(f3)
